e7880c2d1efe0e47bc32a343711938a0cd37921c
aoranow-xun <xun.zheng@aoranow.com>
2024-11-19 15:03:29 +0900

    change react version

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
+import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <HashRouter>
     <App />
-  </React.StrictMode>,
+  </HashRouter>,
   document.getElementById('root')
 );
